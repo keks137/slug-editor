b3c45a34408669d4bfa75afc538b8f51ea731583
keks137 <guntzjonas@gmail.com>
2024-11-04 17:09:38 +0100

    Added save shortcut

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,6 +2,7 @@ extends Node2D
 @onready var editor: CodeEdit = %Editor
 @onready var path_setter: TextEdit = %PathSetter
 
+var file_selected := false
 var work_dir := OS.get_system_dir(OS.SYSTEM_DIR_DOCUMENTS)
 var work_path := OS.get_system_dir(OS.SYSTEM_DIR_DOCUMENTS)
 const SAVE_MENU := preload("res://scenes/save_menu.tscn")
@@ -17,7 +18,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	process_hotkeys()
+	
 
 func create_or_save_file(content):
 	var file = FileAccess.open(work_path, FileAccess.WRITE_READ)
@@ -34,38 +36,48 @@ func load_file(path: String):
 func _on_path_setter_text_changed() -> void:
 	work_path = path_setter.text # Replace with function body.
 
-
-func _on_file_dialog_file_selected(path: String) -> void:
-	print(path) # Replace with function body.
-	if FileAccess.file_exists(path):
-		editor.text = load_file(path)
-	else:
-		editor.text = ""
-	work_path = path
-	path_setter.text = work_path
-
-
 func _on_save_button_pressed() -> void:
-	var save_menu = SAVE_MENU.instantiate()
-	add_child(save_menu)
-	save_menu.connect("file_and_dir_selected", _savefile_and_dir_selected)
-	save_menu.current_dir = work_dir
-	save_menu.current_file = work_path
+	open_save_menu()
 
 
 func _on_open_button_pressed() -> void:
-	var open_menu = OPEN_MENU.instantiate()
-	add_child(open_menu)
-	open_menu.connect("file_and_dir_selected", _openfile_and_dir_selected)
-	open_menu.current_dir = work_dir
-	open_menu.current_file = work_path
+	open_open_menu()
 
 func _savefile_and_dir_selected(path: String, dir: String) -> void:
 	work_path = path
 	work_dir = dir
 	create_or_save_file(editor.text)
+	file_selected = true
 
 func _openfile_and_dir_selected(path: String, dir: String) -> void:
 	work_path = path
 	work_dir = dir
 	editor.text = load_file(work_path)
+	file_selected = true
+
+	
+# Makes Editor ignore certain inputs
+func _on_editor_gui_input(event):
+	if !Input.is_action_just_pressed("save"): return;
+	editor.accept_event()
+
+func open_open_menu() -> void:
+	var open_menu = OPEN_MENU.instantiate()
+	add_child(open_menu)
+	open_menu.connect("file_and_dir_selected", _openfile_and_dir_selected)
+	open_menu.current_dir = work_dir
+	open_menu.current_file = work_path
+	
+func open_save_menu() -> void:
+	var save_menu = SAVE_MENU.instantiate()
+	add_child(save_menu)
+	save_menu.connect("file_and_dir_selected", _savefile_and_dir_selected)
+	save_menu.current_dir = work_dir
+	save_menu.current_file = work_path
+	
+func process_hotkeys() -> void:
+	if Input.is_action_just_pressed("save"):
+		if file_selected:
+			create_or_save_file(editor.text)
+		else:
+			open_save_menu()
